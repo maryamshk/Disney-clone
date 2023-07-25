a47024fbf11dd5ab8cdee09063089c0ba69a2936
Maryam <maryamjaved.xo@gmail.com>
2023-07-25 23:41:25 +0500

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Header>
+        header
+      </Header>
 
+      <Home>
+        home
+      </Home>
     </div>
   );
 }
