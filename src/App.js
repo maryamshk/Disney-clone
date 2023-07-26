55e6ce4d8e7addea645e4bcc192e7511c71fb9e0
Maryam <maryamjaved.xo@gmail.com>
2023-07-26 20:44:28 +0500

    starting detail component and routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,45 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Header>
-        header
-      </Header>
 
-      <Home>
-        home
-      </Home>
+      {/* header will not change other component will change */}
+      {/* anything inside router will always be visible */}
+
+      <Router>
+        <Header />
+        <Routes>
+
+          {/* anything inside switch can always be changed */}
+
+          <Route path="/detail">
+            <Route path='/detail' element={<Detail />} />
+
+          </Route>
+
+          <Route path='/'>
+            <Route path='/' element={<Home />} />
+          </Route>
+
+          {/* <Route path='/login' element={<Login />} /> */}
+          {/* if path is detail we'll gicve it detail page */}
+
+        </Routes>
+      </Router>
+
     </div>
   );
 }
